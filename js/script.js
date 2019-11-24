59b24aab6de6242bbe729489a58557dd2577494c
Piotr Grzybowski <grzybek6@gmail.com>
2019-11-24 11:29:53 +0000

    Add content in script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce to nożyce, to wygrywasz');
